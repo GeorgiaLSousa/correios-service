4a84d0769103b2f8192d466d17f6f8f850879886
Georgia Ligia <georgialigia18@gmail.com>
2025-07-24 21:18:28 -0300

    adicionar serviço de cálculo de frete por UF

---
+++ src/main/java/com/correio/service/service/CalcularFreteService.java
@@ -0,0 +1,45 @@
+package com.correio.service.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalcularFreteService implements FreteService{
+
+    @Override
+    public Double CalcularFrete(String uf) {
+
+        switch (uf.toUpperCase()) {
+            case "SP":
+            case "RJ":
+            case "MG":
+            case "ES":
+                return 7.85; // Região Sudeste
+
+            case "PR":
+            case "SC":
+            case "RS":
+                return 17.30; // Região Sul
+
+            case "DF":
+            case "GO":
+            case "MT":
+            case "MS":
+                return 12.50; // Região Centro-Oeste
+
+            case "AL":
+            case "BA":
+            case "CE":
+            case "MA":
+            case "PB":
+            case "PE":
+            case "PI":
+            case "RN":
+            case "SE":
+                return 15.98; // Região Nordeste
+
+            default:
+                return 20.83; // Região Norte e outros casos
+        }
+    }
+}
---
+++ src/main/java/com/correio/service/service/FreteService.java
@@ -0,0 +1,5 @@
+package com.correio.service.service;
+
+public interface FreteService {
+    Double CalcularFrete(String uf);
+}
