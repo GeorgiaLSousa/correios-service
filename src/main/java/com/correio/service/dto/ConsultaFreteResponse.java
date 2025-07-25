7b297662a3a1a2af3e6ef1a9b02573606bd4786a
Georgia Ligia <georgialigia18@gmail.com>
2025-07-24 21:12:51 -0300

    Criando o DTO da resposta da API, definindo a estrutura dos dados

--- src/main/java/com/correio/service/dto/ConsultaFreteResponse.java
+++ src/main/java/com/correio/service/dto/ConsultaFreteResponse.java
@@ -1,15 +1,20 @@
 package com.correio.service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConsultaFreteResponse {
 
     private String cep;
-    private String rua;
+    private String logradouro;
     private String bairro;
     private String cidade;
-    private String estado;
+    private String uf;
     private double valorFrete;
+
 
 }
